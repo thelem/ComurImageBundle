daa342de52275acc3bc6acd11d5481b327592d92
Ian Tommins <ian@tommins.uk>
2025-02-23 19:52:57 +0000

    Add return type for forward compatibility

--- ComurImageBundle.php
+++ ComurImageBundle.php
@@ -12,7 +12,7 @@ class ComurImageBundle extends Bundle
     /**
      * {@inheritdoc}
      */
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new FormPass());
