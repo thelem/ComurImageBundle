8e78d15aa88ea657152fdc48a2dd66a427af2a55
Ian Tommins <ian@tommins.uk>
2025-02-23 19:45:39 +0000

    Add return type for forward compatibility

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('comur_image');
         $rootNode = method_exists($treeBuilder, "getRootNode")
